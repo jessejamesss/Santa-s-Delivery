f9f7eb597f4ca12b8589c9ec9173328426da9aa7
Aika87 <akris023@ucr.edu>
2020-12-06 11:33:31 -0800

    adding visitor classes

---
+++ visitor.hpp
@@ -0,0 +1,25 @@
+#ifndef __VISITOR_HPP__
+#define __VISITOR_HPP__
+
+#include <iostream> 
+#include "Neighborhood.h"
+#include "Nightmare.h"
+
+using namespace std; 
+
+class Neighborhood; 
+
+class Visitor{
+
+public: 
+      Visitor(){};  
+
+//	Neighborhood* player1 = new Nightmare();
+	
+       virtual void visitWonderland(Neighborhood* n) = 0; 
+       virtual void visitNightmare(Neighborhood* n) = 0;
+
+}; 
+
+
+#endif  
